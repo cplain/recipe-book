f7b4c5ded15a738a9296b739037de62e9076940b
Coby Plain <seaplain@Vivants-MacBook-Pro-3.local>
2013-05-31 15:38:03 +1000

    set up first screen and nav to second

---
+++ RecipieBook/MainMenu.h
@@ -0,0 +1,13 @@
+//
+//  MainMenu.h
+//  RecipieBook
+//
+//  Created by Coby Plain on 31/05/13.
+//  Copyright (c) 2013 Coby Plain. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface MainMenu : NSObject
+
+@end
---
+++ RecipieBook/MainMenu.m
@@ -0,0 +1,13 @@
+//
+//  MainMenu.m
+//  RecipieBook
+//
+//  Created by Coby Plain on 31/05/13.
+//  Copyright (c) 2013 Coby Plain. All rights reserved.
+//
+
+#import "MainMenu.h"
+
+@implementation MainMenu
+
+@end
