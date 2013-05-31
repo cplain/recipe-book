25772bd08ba2a58c76c9887b6c9ecfe38679c383
Coby Plain <seaplain@Vivants-MacBook-Pro-3.local>
2013-05-31 10:45:00 +1000

    Initial Commit

---
+++ RecipieBook/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  RecipieBook
+//
+//  Created by Coby Plain on 31/05/13.
+//  Copyright (c) 2013 Coby Plain. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class ViewController;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) ViewController *viewController;
+
+@end
