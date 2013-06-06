b1d2c1443dabd6ca33909d91b96ab700fc28700a
cplain <coby.plain@gmail.com>
2013-06-06 16:59:45 +1000

    added IBOutlets for text fields

--- RecipieBook/AddPageViewController.h
+++ RecipieBook/AddPageViewController.h
@@ -10,6 +10,13 @@
 
 @interface AddPageViewController : UIViewController
 
+@property (nonatomic, retain) IBOutlet UITextField *nameField;
+@property (nonatomic, retain) IBOutlet UITextField *difficultyField;
+@property (nonatomic, retain) IBOutlet UITextField *prepTimeField;
+@property (nonatomic, retain) IBOutlet UITextField *cookTimeField;
+@property (nonatomic, retain) IBOutlet UITextField *servesField;
+@property (nonatomic, retain) IBOutlet UITextField *ingredientsField;
+@property (nonatomic, retain) IBOutlet UITextField *methodField;
 @property (nonatomic, retain) IBOutlet UIButton *saveButton;
 
 @end
