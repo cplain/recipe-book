a1995bc75ea1ef46e20e82075552524ae9522c92
cplain <coby.plain@gmail.com>
2013-06-07 13:43:39 +1000

    added limited and dodgy view resizing for keyboard, will require more work later for polish

--- RecipieBook/AddPageViewController.h
+++ RecipieBook/AddPageViewController.h
@@ -10,7 +10,9 @@
 
 @interface AddPageViewController : UIViewController
 
+@property (nonatomic, retain) NSMutableArray *recipeList;
 @property (nonatomic, retain) IBOutlet UITextField *nameField;
+@property (nonatomic, retain) IBOutlet UITextField *catergoryField;
 @property (nonatomic, retain) IBOutlet UITextField *difficultyField;
 @property (nonatomic, retain) IBOutlet UITextField *prepTimeField;
 @property (nonatomic, retain) IBOutlet UITextField *cookTimeField;
@@ -18,5 +20,8 @@
 @property (nonatomic, retain) IBOutlet UITextField *ingredientsField;
 @property (nonatomic, retain) IBOutlet UITextField *methodField;
 @property (nonatomic, retain) IBOutlet UIButton *saveButton;
+@property (nonatomic, retain) IBOutlet UIScrollView *scrollView;
+
+-(IBAction)commit:(id)sender;
 
 @end
--- RecipieBook/AddPageViewController.m
+++ RecipieBook/AddPageViewController.m
@@ -10,22 +10,41 @@
 
 @interface AddPageViewController ()
 
+@property (nonatomic) CGRect originalScrollerDimens;
+
+-(void)keyboardWasShown:(NSNotification *)notification;
+-(void)keyboardWillHide:(NSNotification *)notification;
+
 @end
 
 @implementation AddPageViewController
 
+@synthesize recipeList;
+@synthesize nameField;
+@synthesize catergoryField;
+@synthesize difficultyField;
+@synthesize prepTimeField;
+@synthesize cookTimeField;
+@synthesize ingredientsField;
+@synthesize methodField;
 @synthesize saveButton;
+@synthesize scrollView;
 
 - (void)viewDidLoad
 {
     [super viewDidLoad];
     [self addButtonImage:saveButton];
+    [self readPList];
+    
+    [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(keyboardWasShown:) name:UIKeyboardDidShowNotification object:nil];
+    
+    [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(keyboardWillHide:) name:UIKeyboardWillHideNotification object:nil];
+    
 }
 
 - (void)didReceiveMemoryWarning
 {
     [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
 }
 
 - (void)addButtonImage:(UIButton*)button
@@ -35,6 +54,59 @@
     
     [button setBackgroundImage:buttonImage forState:UIControlStateNormal];
     [button setBackgroundImage:buttonImageHighlight forState:UIControlStateHighlighted];
+}
+
+- (void)readPList
+{
+    NSString *plistCatPath = [[NSBundle mainBundle] pathForResource:@"RecipeList" ofType:@"plist"];
+    self.recipeList = [[NSMutableArray arrayWithContentsOfFile:plistCatPath] copy];
+}
+
+-(void)commit:(id)sender
+{
+//    NSArray *paths = NSSearchPathForDirectoriesInDomains (NSDocumentDirectory, NSUserDomainMask, YES);
+//    NSString *documentsPath = [paths objectAtIndex:0];    NSString *plistPath = [documentsPath stringByAppendingPathComponent:@"RecipeList.plist"];
+//    
+//    NSDictionary *recipeDict = [NSDictionary dictionaryWithObjects:[NSArray arrayWithObjects: self.nameField.text, self.catergoryField.text, self.difficultyField.text, self.prepTimeField.text
+//                                                                    , self.cookTimeField.text, self.servesField.text, self.ingredientsField.text, self.methodField.text, nil] forKeys:[NSArray arrayWithObjects: @"Recipe name", @"Catergory", @"Difficulty", @"Time to prep", @"Time to cook", @"Serves", "@Ingredients", @"Method", nil]];
+//    
+//    [self.recipeList addObject:recipeDict];
+//    
+//    NSString *error = nil;
+//    NSData *plistData = [NSPropertyListSerialization dataFromPropertyList:self.recipeList format:NSPropertyListXMLFormat_v1_0 errorDescription:&error];
+//
+//    if(plistData)
+//        [plistData writeToFile:plistPath atomically:YES];
+//    
+//    else
+//        NSLog(@"Error in saveData: %@", error);
+    
+}
+
+-(void)keyboardWasShown:(NSNotification *)notification
+{
+    float viewWidth = self.view.frame.size.width;
+    float viewHeight = self.view.frame.size.height;
+
+    NSDictionary *keyboardInfo = [notification userInfo];
+    CGSize keyboardSize = [[keyboardInfo objectForKey:UIKeyboardFrameBeginUserInfoKey] CGRectValue].size;
+    float keyboardHeight = keyboardSize.height;
+    
+    if (UIDeviceOrientationIsLandscape([[UIDevice currentDevice] orientation])) {
+        keyboardHeight = keyboardSize.width;
+        float temp = viewWidth;
+        viewWidth = viewHeight;
+        viewHeight = temp;
+    }
+    
+    CGRect viewableAreaFrame = CGRectMake(0.0, 0.0, viewWidth, viewHeight - keyboardHeight);
+    [self.scrollView setFrame:viewableAreaFrame];
+    
+}
+
+-(void)keyboardWillHide:(NSNotification *)notification{
+    NSLog(@"Keyboard is closing.");
+    [self.scrollView setFrame:_originalScrollerDimens];
 }
 
 @end
